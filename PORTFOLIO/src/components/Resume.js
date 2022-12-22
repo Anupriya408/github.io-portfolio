e4eb887ff0f3d6103bc59468cde27b7752d1cbbc
Anupriya408 <pihusinha0408@gmail.com>
2022-12-22 21:35:32 +0530

    MY PORTFOLIO

--- PORTFOLIO/src/components/Resume.js
+++ PORTFOLIO/src/components/Resume.js
@@ -8,7 +8,7 @@ function Resume() {
 <button class="btn">
 <h2 className="font-big text-red slide-in-top"> MY RESUME</h2><br></br>
 <img src="./portfolio-images\images.png"></img><br></br>
-<a href="https://drive.google.com/file/d/18jCCemYMNCH_xS0Zuwi-wWm43OkD2X_O/view?usp=share_link">Click here</a></button>
+<a href="https://drive.google.com/file/d/1VXg_Iaw5hlnl7qrOkHlD0FTwraLI1x1V/view?usp=share_link">Click here</a></button>
   )
 }
 export default Resume
